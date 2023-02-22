f1e06999991b3d0c74385c795b93eb20e2817395
ibielecka <iwona.k.michalik@gmail.com>
2023-02-22 09:25:51 +0100

    zrobione

---
+++ main.js
@@ -0,0 +1,11 @@
+function dlugosc(zmienna1, zmienna2) 
+{
+    if (zmienna1.length > zmienna2.length) {
+    console.log(zmienna2);
+    }
+    else {
+    console.log("Krótszy string");
+    }
+}
+
+dlugosc("Uwielbiam JavaScript", "Jestem świetnym programistą");
